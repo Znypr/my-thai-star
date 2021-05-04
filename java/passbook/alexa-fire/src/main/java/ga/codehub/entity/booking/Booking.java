777b547a8e7f53c25356348e98b6169be60a034b
JoostCore <joostcore@gmail.com>
2021-05-04 02:51:20 +0200

    Changed Config and Added the Alexa Project

---
+++ java/passbook/alexa-fire/src/main/java/ga/codehub/entity/booking/Booking.java
@@ -0,0 +1,8 @@
+package ga.codehub.entity.booking;
+
+public class Booking {
+    public String bookingDate;
+    public String name;
+    public String email;
+    public String assistants;
+}
---
+++ java/passbook/alexa-fire/src/main/java/ga/codehub/tools/exceptions/EndOfEntries.java
@@ -0,0 +1,4 @@
+package ga.codehub.tools.exceptions;
+
+public class EndOfEntries extends Exception {
+}
