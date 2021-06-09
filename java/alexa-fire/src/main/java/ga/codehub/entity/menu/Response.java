e5f3a1f13f174cf1c6aa9511250e3bb0baf27c49
Rebecca Hahn <1925729@stud.hs-mannheim.de>
2021-06-09 10:39:39 +0200

    gerichtbeschreibung fix + toStringNames fix über alexa

--- java/alexa-fire/src/main/java/ga/codehub/entity/menu/Response.java
+++ java/alexa-fire/src/main/java/ga/codehub/entity/menu/Response.java
@@ -9,8 +9,16 @@ public class Response {
 
     public String toStringNames() {
         StringBuilder sb = new StringBuilder();
-        for (Content c : content) {
+        /*for (Content c : content) {
             sb.append(c.dish.name + ", ");
+        }*/
+        for(int i = 0; i < content.length; i++){
+            Content c = content[i];
+            if(i == content.length-1){
+                sb.append(c.dish.name);
+            }else{
+                sb.append(c.dish.name + ", ");
+            }
         }
         return sb.toString();
     }
@@ -20,7 +28,21 @@ public class Response {
         for (Content c : content) {
             sb.append(c.dish.description);
         }
-        return sb.toString();
+        String descriptionDefault = sb.toString();
+        String descriptionFinal = "";
+        String[] parts = descriptionDefault.split(" ");
+        for(int i = 0; i < parts.length; i++){
+            String part = parts[i];
+            if(part.equals("&")){
+                part = "and";
+            }
+            if(i == 0){
+                descriptionFinal = part;
+            }else{
+                descriptionFinal += " " + part;
+            }
+        }
+        return descriptionFinal;
     }
 
     public String toStringName() {
