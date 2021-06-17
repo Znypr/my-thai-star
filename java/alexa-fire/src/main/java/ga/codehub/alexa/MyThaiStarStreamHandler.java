3fda3d795bba77d773a29b0eea821ff017905964
dips392 <wpekodpwjoiejip@web.de>
2021-06-17 10:27:13 +0200

    id changed

--- java/alexa-fire/src/main/java/ga/codehub/alexa/MyThaiStarStreamHandler.java
+++ java/alexa-fire/src/main/java/ga/codehub/alexa/MyThaiStarStreamHandler.java
@@ -20,7 +20,8 @@ import ga.codehub.alexa.handlers.*;
 
 public class MyThaiStarStreamHandler extends SkillStreamHandler {
 
-    public static final String BASE_URL = "https://2b455f44cfdd.ngrok.io"; // Rebecca
+    public static final String BASE_URL = "https://0611360415ae.ngrok.io"; // Jannik
+    // public static final String BASE_URL = "https://2b455f44cfdd.ngrok.io"; // Rebecca
     // public static final String BASE_URL = "http://fb6c3671b0bc.ngrok.io"; // Candra
 
     private static Skill getSkill() {
@@ -40,7 +41,8 @@ public class MyThaiStarStreamHandler extends SkillStreamHandler {
                 // Add your skill id below
                 // .withSkillId("amzn1.ask.skill.f5fa69a6-0822-4a5f-b959-ad2893865a9e") //Rebecca at home
                 // .withSkillId("amzn1.ask.skill.b849ee87-c5b0-4a88-ba30-5b823f89ddea") // Candra
-                .withSkillId("amzn1.ask.skill.7d4334f3-c8eb-4fe3-9542-2ebb07ad96e1") //Rebecca inhouse
+                // .withSkillId("amzn1.ask.skill.7d4334f3-c8eb-4fe3-9542-2ebb07ad96e1") //Rebecca inhouse
+                .withSkillId("amzn1.ask.skill.eb6454aa-22a7-4a01-b0c1-6f7b07caee24") //Jannik
                 .build();
     }
 
