81b92d0954b79f955d49dd1904e94448fa95c5f5
Rebecca Hahn <1925729@stud.hs-mannheim.de>
2021-06-24 10:47:25 +0200

    fix

--- java/alexa-fire/src/main/java/ga/codehub/alexa/handlers/AskForHelpIntentHandler.java
+++ java/alexa-fire/src/main/java/ga/codehub/alexa/handlers/AskForHelpIntentHandler.java
@@ -11,6 +11,7 @@ import ga.codehub.entity.booking.Booking;
 import ga.codehub.tools.BasicOperations;
 import ga.codehub.tools.exceptions.Different;
 import ga.codehub.tools.exceptions.NotFound;
+import jdk.internal.org.jline.utils.DiffHelper;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -36,14 +37,16 @@ public class AskForHelpIntentHandler implements RequestHandler {
         String bookingToken = (String) attributes.get("bookingToken");
         String response;
         BasicOperations bo = new BasicOperations();
-        String payload ="";
+        String payload = "";
 
         try {
-            response = bo.basicGET(BASE_URL + "/mythaistar/services/rest/bookingmanagement/v1/booking/help/" + bookingToken);
+            bo.basicGET(BASE_URL + "/mythaistar/services/rest/bookingmanagement/v1/booking/help/" + bookingToken);
             speechText = "Ein Kellner wurde benachrichtigt und wird so bald wie möglich bei dir sein.";
-        } catch (IOException | NotFound | Different e) {
+        } catch (Different diff){
+            speechText = "Ein Kellner wurde benachrichtigt und wird so bald wie möglich bei dir sein.";
+        } catch (NotFound | IOException ex) {
             speechText = "Der my thai star Server scheint Probleme bei der Verarbeitung deiner Anfrage zu haben.";
-            e.printStackTrace();
+           // e.printStackTrace();
         }
 
         return input.getResponseBuilder()
